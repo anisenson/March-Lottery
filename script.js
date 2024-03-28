495ddd4a95df77ec4a731c12bc3080d51347813e
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-03-28 12:56:49 +0000

    Changed HTML Layout !

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
 // create an array to store names
 const namesArray = []
 
+
 function addName() {
-    const nameInput = document.getElementById('nameInput') // storing name input element as a variable
-    const name = nameInput.value.trim() //get the TRIMMED value of the input box and store in a var
-    
-    namesArray.push(name) //add the name to the end of the array
-    displayNames() //call the displayNames function to update the list
-    
-    nameInput.value = '' //Clear the input field after adding the name
+   const nameInput = document.getElementById('nameInput') // storing name input element as a variable
+   const name = nameInput.value.trim() //get the TRIMMED value of the input box and store in a var
+  
+   namesArray.push(name) //add the name to the end of the array
+   displayNames() //call the displayNames function to update the list
+  
+   nameInput.value = '' //Clear the input field after adding the name
 }
+
 
 function displayNames() {
-    const nameList = document.getElementById('nameList')
-    nameList.innerHTML = '' //clear out the previous list items
+   const nameList = document.getElementById('nameList')
+   nameList.innerHTML = '' //clear out the previous list items
 
-    for (let i = 0; i < namesArray.length; i++){
-        const name = namesArray[i]
 
-        const li = document.createElement('li')
-        li.className = 'list-group-item'
+   for (let i = 0; i < namesArray.length; i++){
+       const name = namesArray[i]
 
-        const span = document.createElement('span')
-        span.textContent = name
 
-        li.appendChild(span)//adds a span tag nested between every <li> tag (the child)
-        nameList.appendChild(li)
-    }
+       const li = document.createElement('li')
+       li.className = 'list-group-item'
+
+
+       const span = document.createElement('span')
+       span.textContent = name
+
+
+       li.appendChild(span)//adds a span tag nested between every <li> tag (the child)
+       nameList.appendChild(li)
+   }
+
 
 }
+
 
 // Event listener for the Enter key press
 document.getElementById('nameForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    addName(); // Call the addName function
+   event.preventDefault(); // Prevent the default form submission
+   addName(); // Call the addName function
 });
 
+
 function pickRandomName() {
-    const randomNameDiv = document.getElementById('randomName')
-    randomNameDiv.textContent = ''
+   const randomNameDiv = document.getElementById('randomName')
+   randomNameDiv.textContent = ''
 
-    //Generate and select a random name from names array
-    const randomNumber = Math.floor(Math.random() * namesArray.length)
-    const randomName = namesArray[randomNumber] 
 
-    randomNameDiv.textContent = randomName
+   //Generate and select a random name from names array
+   const randomNumber = Math.floor(Math.random() * namesArray.length)
+   const randomName = namesArray[randomNumber]
 
-    namesArray.splice(randomNumber, 1)
 
-    displayNames(); 
+   randomNameDiv.textContent = randomName
+
+
+   namesArray.splice(randomNumber, 1)
+
+
+   displayNames();
+
 
 }
-
 
 //Event listener for the button click to add a name
 document.getElementById('addNameBtn').addEventListener('click', addName)
 
+
 //Event listener for the button click to select and diplay a rndm name
 document.getElementById('pickRandomBtn').addEventListener('click', pickRandomName)
-
 
